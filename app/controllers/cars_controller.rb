304deb6124c74c4b2e5af1aae4da7e4b0939de3b
Mohammed El Abdi <momo.elabdi@bluewin.ch>
2022-03-22 11:29:52 +0100

    teamplate_new created

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,2 +1,26 @@
 class CarsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:show, :index]
+  def new
+    @car = Car.new()
+  end
+
+  def create
+    @car = List.new(car_params)
+    if @car.save
+      redirect_to car_path(@car)
+    else
+      render :new
+    end
+  end
+
+
+
+
+
+
+  private
+
+  def car_params
+    params.require(:list).permit(:brand, :type, :price)
+  end
 end
--- app/models/car.rb
+++ app/models/car.rb
@@ -1,3 +1,6 @@
 class Car < ApplicationRecord
   belongs_to :user
+  has_many :resevcations
+  validates :brand, :type, :price, presence: true
+
 end
