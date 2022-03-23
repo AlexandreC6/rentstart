06e40d824ad1c4c9c67e0d8cf49153e628934e62
talita chapuis <talita.chapuis@gmail.com>
2022-03-23 14:44:58 +0100

    home3

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,9 +7,10 @@ require 'faker'
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-puts "1"
 Car.delete_all
-puts "deleted."
-3.times do
-  Car.create!(brand: Faker::Lorem.word, fuel: Faker::Lorem.word, description: Faker::Lorem.sentence(word_count: 7), price: Faker::Number.decimal(l_digits: 2), user: User.last)
-end
+
+Car.create!(brand: "Tesla", fuel: "Electrique", description: "blablabla", price: 850.-, user: User.last)
+
+Car.create!(brand: "Toyota", fuel: "Hybrid", description: "Kilometrage illimité. Age minimum 19 ans. Boite automatique. 4 Portes. Émission de CO2: 105g/km", price: 670.-, user: User.last)
+
+Car.create!(brand: "Porshe", fuel: "Diesel", description: "grand reservoir", price: 920.-, user: User.last)
