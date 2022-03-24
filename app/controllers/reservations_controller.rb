442891364e1535a5d761dd1ac0b3e54bbff06473
Alexandre Camilo <alex.camilo6@hotmail.ch>
2022-03-24 15:52:22 +0100

    link new-create-show done

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -5,6 +5,19 @@ class ReservationsController < ApplicationController
   end
 
   def create
+    @reservation = Reservation.new(reservation_params)
+    @car = Car.find(params[:reservation_id])
+    @reservation.car = @car
+    if @reservation.save
+      redirect_to cars_path(@car)
+    else
+      render :new
+    end
+  end
 
+  private
+
+  def reservation_params
+    params.require(:reservation).permit(:start_date, :end_date)
   end
 end
