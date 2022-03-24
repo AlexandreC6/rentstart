9f50af61d938dff62706c3311691c3d9111959f7
Alexandre Camilo <alex.camilo6@hotmail.ch>
2022-03-24 17:27:18 +0100

    remove the raise

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -15,7 +15,7 @@ class ReservationsController < ApplicationController
     @user = current_user.id
     @reservation.user_id = @user
     if @reservation.save
-      raise
+      # raise
       redirect_to car_path(params[:car_id])
     else
       render :new
