f5bda4dcf0c54b33bcec12eb2727d81f2dd483f2
Mohammed El Abdi <momo.elabdi@bluewin.ch>
2022-03-24 14:49:47 +0100

    reservation model routes added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_22_132700) do
+ActiveRecord::Schema.define(version: 2022_03_24_132601) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,17 @@ ActiveRecord::Schema.define(version: 2022_03_22_132700) do
     t.index ["user_id"], name: "index_cars_on_user_id"
   end
 
+  create_table "reservations", force: :cascade do |t|
+    t.bigint "car_id", null: false
+    t.bigint "user_id", null: false
+    t.date "start_date"
+    t.date "end_date"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["car_id"], name: "index_reservations_on_car_id"
+    t.index ["user_id"], name: "index_reservations_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -39,4 +50,6 @@ ActiveRecord::Schema.define(version: 2022_03_22_132700) do
   end
 
   add_foreign_key "cars", "users"
+  add_foreign_key "reservations", "cars"
+  add_foreign_key "reservations", "users"
 end
