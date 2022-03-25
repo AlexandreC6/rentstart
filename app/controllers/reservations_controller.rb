0916b7614e8253a0b7632b3c984a9e2775dd4621
Alexandre Camilo <alex.camilo6@hotmail.ch>
2022-03-25 11:29:02 +0100

    problem heroku

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,20 +1,20 @@
 class ReservationsController < ApplicationController
   def my_reservations
-    @reservation = Reservation.all
+    @reservations = Reservation.all
   end
 
   def new
     @car = Car.find(params[:car_id])
-    @reservation = Reservation.new
+    @reservations = Reservation.new
   end
 
   def create
-    @reservation = Reservation.new(reservation_params)
+    @reservations = Reservation.new(reservation_params)
     @car = Car.find(params[:car_id])
-    @reservation.car = @car
+    @reservations.car = @car
     @user = current_user.id
-    @reservation.user_id = @user
-    if @reservation.save
+    @reservations.user_id = @user
+    if @reservations.save
       # raise
       redirect_to car_path(params[:car_id])
     else
@@ -25,7 +25,7 @@ class ReservationsController < ApplicationController
   private
 
   def reservation_params
-    params.require(:reservation).permit(:start_date, :end_date)
+    params.require(:reservations).permit(:start_date, :end_date)
   end
 
 end
