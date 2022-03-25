4766ae657b544993476486abec2e02e3b50dd92d
Alexandre Camilo <alex.camilo6@hotmail.ch>
2022-03-25 12:51:33 +0100

    before heroku

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :cars, only: [:new, :create, :index, :show] do
-    resources :reservations, only: [ :new, :create ]
+    resources :reservations, only: [ :new, :create, :edit, :update ]
   end
   get "/my_reservations", to: "reservations#my_reservations"
 end
